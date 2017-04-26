6680f89626f4e6879fc27b66616f0934765cdfd4
dkumagai <doug@mapletonhillmedia.com>
2017-04-26 13:49:23 -0600

    initial commit

---
+++ BuildTest2015/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuildTest2015.Startup))]
+namespace BuildTest2015
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
