343b43fd435c4324bf7c5bc770ce0d07a571affd
dkumagai <doug@mapletonhillmedia.com>
2017-05-08 11:04:21 -0600

    adding front-end build step

---
+++ BuildTest2015/gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require("gulp");
+const debug = require("gulp-debug");
+const sass = require("gulp-sass");
+
+gulp.task("default", () => {
+	gulp.src("Content/Sass/main.scss")
+		.pipe(debug())
+		.pipe(sass())
+		.pipe(gulp.dest("Content/css"));
+});
